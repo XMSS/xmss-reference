XMSS/xmss-reference

commit 719cb467df0187cf29b03b94f7b8d95fec613ab4

Prevent allocating for a 'next' tree on top layer

xmss_fast.c
@@ -983,7 +983,7 @@ int xmssmt_sign(unsigned char *sk, bds_state *states, unsigned char *wots_sigs, 
         }
       }
     }
-    else {
+    else if (idx < (1UL << h) - 1) {
       memcpy(&tmp, states+params->d + i, sizeof(bds_state));
       memcpy(states+params->d + i, states + i, sizeof(bds_state));
       memcpy(states + i, &tmp, sizeof(bds_state));

test/test_xmssmt_fast.c
@@ -31,17 +31,18 @@ int main()
   unsigned int tree_h = h / d;
 
   // stack needs to be larger than regular (H-K-1), since we re-use for 'next'
-  unsigned char stack[2*d * (tree_h + 1)*n];
-  unsigned char stacklevels[2*d * (tree_h + 1)*n];
-  unsigned char auth[2*d * tree_h*n];
-  unsigned char keep[2*d * (tree_h >> 1)*n];
-  treehash_inst treehash[2*d * (tree_h-k)];
-  unsigned char th_nodes[2*d * (tree_h-k)*n];
-  unsigned char retain[2*d * ((1 << k) - k - 1)*n];
+  unsigned char stack[(2*d-1) * (tree_h + 1)*n];
+  unsigned char stacklevels[(2*d-1) * (tree_h + 1)*n];
+  unsigned char auth[(2*d-1) * tree_h*n];
+  unsigned char keep[(2*d-1) * (tree_h >> 1)*n];
+  treehash_inst treehash[(2*d-1) * (tree_h-k)];
+  unsigned char th_nodes[(2*d-1) * (tree_h-k)*n];
+  unsigned char retain[(2*d-1) * ((1 << k) - k - 1)*n];
   unsigned char wots_sigs[d * params->xmss_par.wots_par.keysize];
-  bds_state states[2*d]; // first d are 'regular' states, second d are 'next'
+  // first d are 'regular' states, second d are 'next'; top tree has no 'next'
+  bds_state states[2*d-1];
 
-  for (i = 0; i < 2*d; i++) {
+  for (i = 0; i < 2*d-1; i++) {
     for(j=0;j<tree_h-k;j++)
       treehash[i*(tree_h-k) + j].node = th_nodes + (i*(tree_h-k) + j) * n;
     xmss_set_bds_state(states + i,

commit e2f1cee548897854ed5bc5b5a450b77c23980074

Fix error for d=1 cases as introduced by 719cb46

xmss_fast.c
@@ -948,8 +948,10 @@ int xmssmt_sign(unsigned char *sk, bds_state *states, unsigned char *wots_sigs, 
 
   SET_LAYER_ADDRESS(addr, 0);
   SET_TREE_ADDRESS(addr, (idx_tree + 1));
-  // mandatory update for NEXT_0 (does not count towards h-k/2)
-  bds_state_update(&states[params->d], sk_seed, &(params->xmss_par), pub_seed, addr);
+  // mandatory update for NEXT_0 (does not count towards h-k/2) if NEXT_0 exists
+  if ((1 + idx_tree) * (1 << tree_h) + idx_leaf < (1UL << h)) {
+    bds_state_update(&states[params->d], sk_seed, &(params->xmss_par), pub_seed, addr);
+  }
 
   for (i = 0; i < params->d; i++) {
     // check if we're not at the end of a tree