80815ff4c44e2d317997312970c0ef1c663aa7cf
Duc Nguyen <ductri.nguyen@sandboxquantum.com>
2022-06-15 13:38:13 -0400

    add DEBUG for printf

--- nist.c
+++ nist.c
@@ -14,21 +14,27 @@ int crypto_sign_keypair(unsigned char *pk, unsigned char *sk)
     ret |= XMSS_STR_TO_OID(&oid, XMSS_OID);
     if (ret)
     {
+#if DEBUG
         printf("Did not recognize %s!\n", XMSS_OID);
+#endif
         return OQS_ERROR;
     }
 
     ret |= XMSS_PARSE_OID(&params, oid);
     if (ret)
     {
+#if DEBUG
         printf("Could not parse OID for %s!\n", XMSS_OID);
+#endif
         return OQS_ERROR;
     }
 
     ret |= XMSS_KEYPAIR(pk, sk, oid);
     if (ret)
     {
+#if DEBUG
         printf("Error generating keypair %d\n", ret);
+#endif
         return OQS_ERROR;
     }
 
@@ -41,7 +47,9 @@ int crypto_sign(unsigned char *sm, unsigned long long *smlen,
     int ret = XMSS_SIGN(sk, sm, smlen, m, mlen);
     if (ret)
     {
+#if DEBUG
         printf("Error generating signature %d\n", ret);
+#endif
         return OQS_ERROR;
     }
 
@@ -54,7 +62,9 @@ int crypto_sign_open(unsigned char *m, unsigned long long *mlen,
     int ret = XMSS_SIGN_OPEN(m, mlen, sm, smlen, pk);
     if (ret)
     {
+#if DEBUG
         printf("Error verifying signature %d\n", ret);
+#endif
         return OQS_ERROR;
     }
 
@@ -66,7 +76,9 @@ int crypto_remain_signatures(unsigned long long *remain,
 {
     if (XMSS_REMAIN_SIG(remain, max, sk))
     {
+#if DEBUG
         printf("Error counting remaining signatures\n");
+#endif
         return OQS_ERROR;
     }
     return OQS_SUCCESS;
