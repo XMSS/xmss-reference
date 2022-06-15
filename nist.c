9cd0ab6a2168a00b6a71a33fcb11ea000bf58be8
Duc Nguyen <ductri.nguyen@sandboxquantum.com>
2022-06-15 14:23:28 -0400

    add header document

--- nist.c
+++ nist.c
@@ -5,6 +5,18 @@
 #include "nist_params.h"
 #include "xmss.h"
 
+/*************************************************
+ * Name:        crypto_sign_keypair
+ *
+ * Description: Generates public and private key.
+ *
+ * Arguments:   - uint8_t *pk: pointer to output public key (allocated
+ *                             array of LMS_CRYPTO_PUBLICKEYBYTES bytes)
+ *              - uint8_t *sk: pointer to output private key (allocated
+ *                             array of LMS_CRYPTO_SECRETKEYBYTES bytes)
+ *
+ * Returns 0 (success), -1 otherwise
+ **************************************************/
 int crypto_sign_keypair(unsigned char *pk, unsigned char *sk)
 {
     xmss_params params;
@@ -41,6 +53,19 @@ int crypto_sign_keypair(unsigned char *pk, unsigned char *sk)
     return OQS_SUCCESS;
 }
 
+/*************************************************
+ * Name:        crypto_sign
+ *
+ * Description: Computes signature.
+ *
+ * Arguments:   - uint8_t *sm:   pointer to output signature (of length CRYPTO_BYTES)
+ *              - uint8_t *m:    pointer to message to be signed
+ *              - uint8_t *sk:   pointer to bit-packed secret key
+ *              - unsigned long long *smlen: pointer to output length of signature
+ *              - unsigned long long mlen:   length of message
+ *
+ * Returns 0 (success), -1 otherwise
+ **************************************************/
 int crypto_sign(unsigned char *sm, unsigned long long *smlen,
                 const unsigned char *m, unsigned long long mlen, unsigned char *sk)
 {
@@ -56,6 +81,20 @@ int crypto_sign(unsigned char *sm, unsigned long long *smlen,
     return OQS_SUCCESS;
 }
 
+/*************************************************
+ * Name:        crypto_sign_open
+ *
+ * Description: Verify signed message.
+ *
+ * Arguments:   - uint8_t *m: pointer to output message (allocated
+ *                            array with smlen bytes), can be equal to sm
+ *              - const uint8_t *sm: pointer to signed message
+ *              - const uint8_t *pk: pointer to bit-packed public key
+ *              - unsigned long long *mlen: pointer to output length of message
+ *              - unsigned long long smlen: length of signed message
+ *
+ * Returns 0 if signed message could be verified correctly and -1 otherwise
+ **************************************************/
 int crypto_sign_open(unsigned char *m, unsigned long long *mlen,
                      const unsigned char *sm, unsigned long long smlen, const unsigned char *pk)
 {
@@ -71,6 +110,17 @@ int crypto_sign_open(unsigned char *m, unsigned long long *mlen,
     return OQS_SUCCESS;
 }
 
+/*************************************************
+ * Name:        crypto_remain_signatures
+ *
+ * Description: Return number of signature left
+ *
+ * Arguments:   - unsigned long long *remain: remaining signatures
+ *              - unsigned long long *max: maximum number of possibile signature
+ *              - const uint8_t *sk: pointer to bit-packed private key
+ *
+ * Returns 0 (sucess), -1 otherwise
+ **************************************************/
 int crypto_remain_signatures(unsigned long long *remain,
                              unsigned long long *max, const unsigned char *sk)
 {
