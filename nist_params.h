222eb66d31a4aea1164dfc74b93814aa752b422d
Duc Nguyen <ductri.nguyen@sandboxquantum.com>
2022-06-23 11:19:54 -0400

    refactor param, remove NIST_LEVEL

--- nist_params.h
+++ nist_params.h
@@ -4,105 +4,109 @@
 #include "params.h"
 
 
-#ifndef NIST_LEVEL
-#define NIST_LEVEL 0
+#ifndef TREE_LEVEL
+#define TREE_LEVEL 1
 #endif
 
-/*
- * NIST_LEVEL below does not make sense in term of security
- * I use it as place holder for the total number of signatures
- */
-
-/* 
- * Maximum signatures: 2^h = 2^10
- */
-#if NIST_LEVEL == 0
-
-#define XMSS_OID "XMSS-SHA2_10_256"
+#ifndef XMSSMT
 #define XMSSMT 0
+#endif
 
-#define XMSS_PUBLICKEYBYTES 64
-#define XMSS_SECRETKEYBYTES_SLOW 132
-#define XMSS_SECRETKEYBYTES 1373
+#if XMSSMT == 0
+    /* 
+    * Maximum signatures: 2^h = 2^10
+    */
+    #if TREE_LEVEL == 0
 
-#define XMSS_SIGNBYTES 2500
+    #define XMSS_OID "XMSS-SHA2_10_256"
 
-/* 
- * Maximum signatures: 2^h = 2^16
- */
-#elif NIST_LEVEL == 1
+    #define XMSS_PUBLICKEYBYTES 64
+    #define XMSS_SECRETKEYBYTES_SLOW 132
+    #define XMSS_SECRETKEYBYTES 1373
 
-#define XMSS_OID "XMSS-SHA2_16_256"
-#define XMSSMT 0
+    #define XMSS_SIGNBYTES 2500
 
-#define XMSS_PUBLICKEYBYTES 64
-#define XMSS_SECRETKEYBYTES_SLOW 132
-#define XMSS_SECRETKEYBYTES 2093
+    /* 
+    * Maximum signatures: 2^h = 2^16
+    */
+    #elif TREE_LEVEL == 1
 
-#define XMSS_SIGNBYTES 2692
+    #define XMSS_OID "XMSS-SHA2_16_256"
 
-/* 
- * Maximum signatures: 2^h = 2^20
- */
-#elif NIST_LEVEL == 2
+    #define XMSS_PUBLICKEYBYTES 64
+    #define XMSS_SECRETKEYBYTES_SLOW 132
+    #define XMSS_SECRETKEYBYTES 2093
 
-#define XMSS_OID "XMSS-SHA2_20_256"
-#define XMSSMT 0
+    #define XMSS_SIGNBYTES 2692
 
-#define XMSS_PUBLICKEYBYTES 64
-#define XMSS_SECRETKEYBYTES_SLOW 132
-#define XMSS_SECRETKEYBYTES 2573
+    /* 
+    * Maximum signatures: 2^h = 2^20
+    */
+    #elif TREE_LEVEL == 2
 
-#define XMSS_SIGNBYTES 2820
+    #define XMSS_OID "XMSS-SHA2_20_256"
 
-/* 
- * Maximum signatures: 2^h = 2^20
- * XMSS^MT has bigger signature and secret key (secret is not transfer), but better speed
- */
-#elif NIST_LEVEL == 3
+    #define XMSS_PUBLICKEYBYTES 64
+    #define XMSS_SECRETKEYBYTES_SLOW 132
+    #define XMSS_SECRETKEYBYTES 2573
 
-#define XMSS_OID "XMSSMT-SHA2_20/2_256"
-#define XMSSMT 1
+    #define XMSS_SIGNBYTES 2820
 
-#define XMSS_PUBLICKEYBYTES 64
-#define XMSS_SECRETKEYBYTES_SLOW 131
-#define XMSS_SECRETKEYBYTES 5998
 
-#define XMSS_SIGNBYTES 4963
+    #else
 
-/* 
- * Maximum signatures: 2^h = 2^40
- * XMSS^MT has bigger signature and secret key (secret is not transfer), but better speed
- */
-#elif NIST_LEVEL == 4
+    #error "Unspecified TREE_LEVEL {0,1,2}"
 
-#define XMSS_OID "XMSSMT-SHA2_40/2_256"
-#define XMSSMT 1
+    #endif
+#else 
+    /* 
+    * Maximum signatures: 2^h = 2^20
+    * XMSS^MT has bigger signature and secret key (secret is not transfer), but better speed
+    */
+    #if TREE_LEVEL == 0
 
-#define XMSS_PUBLICKEYBYTES 64
-#define XMSS_SECRETKEYBYTES_SLOW 133
-#define XMSS_SECRETKEYBYTES 9600
+    #define XMSS_OID "XMSSMT-SHA2_20/2_256"
 
-#define XMSS_SIGNBYTES 5605
+    #define XMSS_PUBLICKEYBYTES 64
+    #define XMSS_SECRETKEYBYTES_SLOW 131
+    #define XMSS_SECRETKEYBYTES 5998
 
-/* 
- * Maximum signatures: 2^h = 2^60
- * XMSS^MT has bigger signature and secret key (secret is not transfer), but better speed
- */
-#elif NIST_LEVEL == 5
+    #define XMSS_SIGNBYTES 4963
 
-#define XMSS_OID "XMSSMT-SHA2_60/3_256"
-#define XMSSMT 1
+    /* 
+    * Maximum signatures: 2^h = 2^40
+    * XMSS^MT has bigger signature and secret key (secret is not transfer), but better speed
+    */
+    #elif TREE_LEVEL == 1
 
-#define XMSS_PUBLICKEYBYTES 64
-#define XMSS_SECRETKEYBYTES_SLOW 136
-#define XMSS_SECRETKEYBYTES 16629
+    #define XMSS_OID "XMSSMT-SHA2_40/2_256"
 
-#define XMSS_SIGNBYTES 8392
+    #define XMSS_PUBLICKEYBYTES 64
+    #define XMSS_SECRETKEYBYTES_SLOW 133
+    #define XMSS_SECRETKEYBYTES 9600
 
-#else
+    #define XMSS_SIGNBYTES 5605
 
-#error "Unspecified NIST_LEVEL {0,1,2,3,4,5}"
+    /* 
+    * Maximum signatures: 2^h = 2^60
+    * XMSS^MT has bigger signature and secret key (secret is not transfer), but better speed
+    */
+    #elif TREE_LEVEL == 2
+
+    #define XMSS_OID "XMSSMT-SHA2_60/3_256"
+
+    #define XMSS_PUBLICKEYBYTES 64
+    #define XMSS_SECRETKEYBYTES_SLOW 136
+    #define XMSS_SECRETKEYBYTES 16629
+
+    #define XMSS_SIGNBYTES 8392
+
+
+    #else
+
+    #error "Unspecified TREE_LEVEL {0,1,2}"
+
+    #endif
 
 #endif
 
